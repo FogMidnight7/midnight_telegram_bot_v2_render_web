1ed849164a539cdd3ebb5e3056e30f2199369514
FogMidnight7 <edge.midnight@gmail.com>
2025-11-10 06:39:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,11 +68,12 @@ async def cmd_getid(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app.add_handler(CommandHandler("getid", cmd_getid))
 
 
-    # schedules
-    jq = app.job_queue
-    jq.run_daily(send_affirmation, time=time(6, 0, tzinfo=TZ), name="daily_aff")
-    jq.run_daily(send_motivation, time=time(13, 0, tzinfo=TZ), name="daily_motivate")
-    jq.run_daily(send_weekly_recap, time=time(20, 0, tzinfo=TZ), days=(6,), name="weekly_recap")
+# schedules
+jq = app.job_queue
+jq.run_daily(send_affirmation, time=time(6, 0, tzinfo=TZ), name="daily_aff")
+jq.run_daily(send_motivation, time=time(13, 0, tzinfo=TZ), name="daily_motivate")
+jq.run_daily(send_weekly_recap, time=time(20, 0, tzinfo=TZ), days=(6,), name="weekly_recap")
+
 
     # start polling + stay alive
     await app.initialize()
